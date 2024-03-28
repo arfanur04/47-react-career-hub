eb7b8982190a6ff8c93fca2e23782550f696e87e
arfanur04 <arfanur04@gmail.com>
2024-03-29 01:26:42 +0600

    47-5

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { TiLocationOutline } from "react-icons/ti";
+import { CiDollar } from "react-icons/ci";
+
 const Job = ({ job }) => {
 	const {
 		logo,
@@ -7,7 +10,7 @@ const Job = ({ job }) => {
 		location,
 		job_type,
 		salary,
-		job_description,
+		// job_description,
 	} = job;
 
 	return (
@@ -28,6 +31,16 @@ const Job = ({ job }) => {
 					<button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
 						{job_type}
 					</button>
+				</div>
+				<div className="flex mt-4">
+					<h2 className="flex mr-4">
+						<TiLocationOutline className="mr-2 text-2xl" />
+						{location}
+					</h2>
+					<h2 className="flex">
+						<CiDollar className="text-2xl" />
+						{salary}
+					</h2>
 				</div>
 				<div className="card-actions">
 					<button className="btn btn-primary">View Details</button>
